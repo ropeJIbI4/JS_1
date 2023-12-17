b7f914ee5f37ac6a60d4b2141567ab831abeb4e8
ropeJIbI4 <pogi199411@gmail.com>
2023-12-17 21:51:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let Celsia = prompt();
+let farengeit = ((9/5)*Celsia+32);
+console.log(`Это Фаренгейты : ${farengeit}`);
+console.log(`Это Цельсия : ${Celsia}`);
+
+
+let name = 'Alex';
+let admin = name;
+console.log(admin);
